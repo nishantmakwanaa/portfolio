36acf456a2f0046c45b900bb86a3f56249b75592
Nishant Makwana <nishantmakwanacreations@gmail.com>
2025-11-30 11:04:06 +0530

    APLHA

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import './App.css';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import About from './pages/About';
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,9 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'ion-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { name?: string; src?: string; class?: string };
+import React from 'react';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'ion-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { name?: string; src?: string; class?: string };
+        }
     }
 }
