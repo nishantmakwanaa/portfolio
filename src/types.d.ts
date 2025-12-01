11c63d2404760ec03b6ab4ddfc68c50fb90530e2
Nishant Makwana <nishantmakwanacreations@gmail.com>
2025-12-01 22:14:06 +0530

    CHANGES

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,15 @@
+import React from 'react';
+
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            'ion-icon': {
+            'ion-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
                 name?: string;
                 src?: string;
-                class?: string;
-                className?: string;
                 size?: string;
                 color?: string;
                 'aria-hidden'?: boolean | 'true' | 'false';
-                style?: React.CSSProperties;
+                class?: string;
             };
         }
     }
